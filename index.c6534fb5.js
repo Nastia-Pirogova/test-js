ab32818d47023cc7183dbf9d52a0060fbb555386
Nastia-Pirogova <109249070+Nastia-Pirogova@users.noreply.github.com>
2023-06-02 17:57:19 +0000

    Deploying to gh-pages from @ Nastia-Pirogova/test-js@b8d6ef21d0ee7576bc13dd2986939750a9a4dffc 🚀

--- index.c6534fb5.js
+++
@@ -1,2 +0,0 @@
-let e=document.getElementById("dropdownMenuLibrary"),t=document.querySelector(".dropdown-toggle-library"),l=document.querySelector(".dropdown-menu-library");t.addEventListener("click",(function(){t.classList.toggle("rotated"),"none"===e.style.display||""===e.style.display?e.style.display="block":e.style.display="none"})),l.addEventListener("click",(function(e){let t=e.target;t&&t.classList.toggle(".dropdown-item-library-color")}));
-//# sourceMappingURL=index.c6534fb5.js.map
